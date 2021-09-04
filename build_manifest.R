fa0b4773031dbf1e3cbee2eabe40cec29af868ca
Danielle Navarro <d.navarro@unsw.edu.au>
2021-09-05 09:00:32 +1000

    adds date to manifest

--- build_manifest.R
+++ build_manifest.R
@@ -21,31 +21,34 @@ manifest <- tibble::tibble(path = paths) %>%
     into = c("series", "sys_id", "img_id", "short_name"),
     sep = "_"
   ) %>%
-  dplyr::mutate(
-    resolution = as.integer(resolution),
-    long_name = dplyr::case_when(
-      short_name == "incursions" ~ "Incursions",
-      short_name == "inferno" ~ "Seventh Circle",
-      short_name == "percolate" ~ "Percolate",
-      short_name == "teacup-ocean" ~ "Ocean in a Teacup",
-      short_name == "gentle-descent" ~ "Gentle Descent",
-      short_name == "stormy-seas" ~ "Stormy Seas",
-      short_name == "turmeric" ~ "Turmeric Against Grey Tuesday",
-      short_name == "torn-and-frayed" ~ "Torn and Frayed",
-      short_name == "storm-cell" ~ "Storm Cell / Air Elemental",
-      short_name == "tonal-earth" ~ "Tonal Earth",
-      short_name == "cold-front" ~ "Cold Front",
-      short_name == "kintsugi-dreams" ~ "Kintsugi Dreams",
-      short_name == "departure" ~ "Departure",
-      short_name == "echo" ~ "Echo",
-      short_name == "portal" ~ "Portal",
-      short_name == "salt-stone-storm" ~ "Gods of Salt, Stone, and Storm",
-      short_name == "amanecer-de-invierno" ~ "El Último Amanecer de Invierno",
-      short_name == "woodland-spirits" ~ "Woodland Spirits",
-      short_name == "plume" ~ "Plume",
-      short_name == "below-the-horizon" ~ "Below the Horizon",
-      TRUE ~ NA_character_
-    )
-  )
+  dplyr::mutate(resolution = as.integer(resolution))
+
+extras <- tibble::tribble(
+  ~short_name, ~long_name, ~date,
+  "incursions", "Incursions", "2021-08-14",
+  "inferno", "Seventh Circle", "2021-08-27",
+  "percolate", "Percolate", "2021-08-21",
+  "teacup-ocean", "Ocean in a Teacup", "2021-07-31",
+  "gentle-descent", "Gentle Descent", "2021-08-21",
+  "stormy-seas", "Stormy Seas", "2021-08-22",
+  "turmeric", "Turmeric Against Grey Tuesday", "2021-08-24",
+  "torn-and-frayed", "Torn and Frayed", "2021-08-24",
+  "storm-cell", "Storm Cell / Air Elemental", "2021-08-27",
+  "tonal-earth", "Tonal Earth", "2021-08-29",
+  "cold-front", "Cold Front", "2021-08-29",
+  "kintsugi-dreams", "Kintsugi Dreams", "2021-08-29",
+  "departure", "Departure", "2021-08-29",
+  "echo", "Echo", "2021-08-30",
+  "portal", "Portal", "2021-08-31",
+  "salt-stone-storm", "Gods of Salt, Stone, and Storm", "2021-08-31",
+  "amanecer-de-invierno", "El Último Amanecer de Invierno", "2021-09-01",
+  "woodland-spirits", "Woodland Spirits", "2021-09-02",
+  "plume", "Plume", "2021-09-02",
+  "below-the-horizon", "Below the Horizon", "2021-09-03"
+)
+
+manifest <- manifest %>%
+  dplyr::left_join(extras) %>%
+  dplyr::arrange(date, img_id)
 
 readr::write_csv(manifest, here::here("docs", "manifest.csv"))
